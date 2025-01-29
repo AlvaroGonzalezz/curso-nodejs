182593b96739a510db39df63e09c7a3a0c36cfec
AlvaroGlz <alvarogonzalez0509@gmail.com>
2025-01-29 12:51:53 -0600

    Se realizo el primer archivo ES modules

---
+++ cjs/sum.js
@@ -0,0 +1,9 @@
+function sum(n1, n2) {
+    return n1 + n2
+}
+
+// Module CommonJS export
+// module.exports = sum
+module.exports = {
+    sum
+}
---
+++ mjs/index.mjs
@@ -0,0 +1,6 @@
+// ES modules imported
+// import {sum} from './sum.mjs';
+import {sum, rest} from './sum.mjs';
+
+console.log(sum(1,2));
+console.log(rest(1,2));
